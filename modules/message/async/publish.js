ee74263ef9a0ad6a93760de3e5010a7df75eb95f
Gordon Martin <gordon.hugh.martin@gmail.com>
2018-02-13 22:33:08 +0000

    Show warning when posting, but you have no followers.

--- modules/message/async/publish.js
+++ modules/message/async/publish.js
@@ -1,32 +1,52 @@
 var h = require('mutant/h')
 var nest = require('depnest')
+var when = require('mutant/when')
 
 exports.needs = nest({
+  'keys.sync.id': 'first',
   'sheet.display': 'first',
   'message.html.render': 'first',
   'sbot.async.publish': 'first',
-  'keys.sync.id': 'first',
-  'intl.sync.i18n': 'first'
+  'intl.sync.i18n': 'first',
+  'profile.obs.contact': 'first'
 })
 
 exports.gives = nest('message.async.publish')
 
 exports.create = function (api) {
+
   const i18n = api.intl.sync.i18n
   return nest('message.async.publish', function (content, cb) {
     api.sheet.display(function (close) {
+      const contact = api.profile.obs.contact(api.keys.sync.id())
+
       return {
         content: [
           api.message.html.render({value: {
             content,
             private: !!content.recps,
             author: api.keys.sync.id()
-          }})
+          }}),
+          renderFollowerWarning()
         ],
         footer: [
           h('button -save', { 'ev-click': publish }, i18n('Confirm')),
           h('button -cancel', { 'ev-click': cancel }, i18n('Cancel'))
         ]
+      }
+
+      function renderFollowerWarning() {
+        var warning = i18n("You have no followers. Your message will not be visible to anyone until you have at least one follower.")
+        var pubHelp = i18n("The easiest way to get a follower is to redeem an invite code from a pub, which will follow you. See https://www.scuttlebutt.nz/getting-started.html for help. If the pub does not follow you back, try another one.")
+        var moreInfo = i18n("Note: If you post your message, it will become visible to others later when you have followers.")
+
+        var content = h('div', [
+          h('div', warning),
+          h('div', pubHelp),
+          h('div', moreInfo)
+        ])
+
+        return when(contact.hasNoFollowers, content)
       }
 
       function publish () {
